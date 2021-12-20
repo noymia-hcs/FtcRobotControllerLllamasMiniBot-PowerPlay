3855726e9f9a166b874b501cfa5af8a95d1d056b
Chris Kunkel <ckunkel@hobokencs.net>
2021-12-20 16:47:45 -0500

    update auot and teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedHubWarehouseBarcode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedHubWarehouseBarcode.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@Autonomous(name = "BLUEDuckHubWarehouseBarcode", group = "18051")
-public class BlueDuckHubWarehouseBarcode extends LinearOpMode {
+@Autonomous(name = "REDHubParkBarcode", group = "18051")
+public class RedHubWarehouseBarcode extends LinearOpMode {
 
     LlamaBot robot = new LlamaBot();
 
@@ -16,32 +16,33 @@ public class BlueDuckHubWarehouseBarcode extends LinearOpMode {
         waitForStart();
 
         robot.closeClaw(500);
-        robot.armMoveToPosition(LlamaBot.ARM_POSITION_L3_DRIVE, this);
+        robot.armMoveToPosition(LlamaBot.ARM_POSITION_L1_DRIVE, this);
         robot.driveForwardByTime(1, 800);
 
         int dropPosition = LlamaBot.ARM_POSITION_L3_DROP;
 
         if (robot.distance.getDistance(DistanceUnit.CM) < 35) {
-            // L3
-            dropPosition = LlamaBot.ARM_POSITION_L3_DROP;
+            // L1
+            dropPosition = LlamaBot.ARM_POSITION_L1_DROP;
         } else {
-            robot.strafeLeftByTime(0.3, 3000);
+            robot.strafeRightByTime(0.3, 3000);
             double distance = robot.distance.getDistance(DistanceUnit.CM);
-            robot.strafeRightByTime(0.3, 2500);
+            robot.strafeLeftByTime(0.3, 2200);
 
             if (distance < 35) {
                 // L2
                 robot.armMoveToPosition(LlamaBot.ARM_POSITION_L2_DRIVE, this);
                 dropPosition = LlamaBot.ARM_POSITION_L2_DROP;
             } else {
-                // L1
-                robot.armMoveToPosition(LlamaBot.ARM_POSITION_L1_DRIVE, this);
-                dropPosition = LlamaBot.ARM_POSITION_L1_DROP;
+                // L3
+                robot.armMoveToPosition(LlamaBot.ARM_POSITION_L3_DRIVE, this);
+                dropPosition = LlamaBot.ARM_POSITION_L3_DROP;
             }
         }
         robot.driveForwardByTime(-1, 800);
         robot.driveForwardByTime(-0.3, 200);
 
+        robot.strafeLeftByTime(1, 1000);
 
         if (dropPosition == LlamaBot.ARM_POSITION_L3_DROP) {
             robot.driveForwardByTime(1/*robot.DRIVE_SPEED * 4*/, 1200);
@@ -51,15 +52,8 @@ public class BlueDuckHubWarehouseBarcode extends LinearOpMode {
         robot.openClaw(300);
         robot.driveForwardByTime(-1, 1300);
         robot.driveForwardByTime(-0.3, 600);
-        robot.driveForwardByTime(1, 214);
-        robot.strafeRightByTime(1, 1300);
-        robot.strafeRightByTime(0.3, 300);
-        robot.spin(true, 4200);
-        robot.driveForwardByTime(1, 642);
-        robot.strafeRightByTime(1, 800);
-
-
-
+        robot.strafeRightByTime(robot.DRIVE_SPEED * 4, 3700);
+        robot.armMoveToPosition(LlamaBot.ARM_POSITION_FLOOR, this);
     }
 }
 
